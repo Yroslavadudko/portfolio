517a9555cd1b9f1495facd13a2830884f11e3160
Yroslavadudko <dudkomykola@icloud.com>
2023-07-30 11:38:47 +0300

    delete bootstrap

--- app.js
+++ app.js
@@ -62,9 +62,4 @@ document
       });
   });
 
-// Bootstrap
-import { Toast } from "bootstrap.esm.min.js";
 
-Array.from(document.querySelectorAll(".toast")).forEach(
-  (toastNode) => new Toast(toastNode)
-);
