9bf5e3fe5d0a7273e2abba33e73c6a5e3c9f0775
Yroslavadudko <dudkomykola@icloud.com>
2023-08-24 21:35:41 +0300

    Hello, clamp

--- cypress/e2e/test_4-pagination.cy.js
+++ cypress/e2e/test_4-pagination.cy.js
@@ -7,27 +7,31 @@ describe("My Test Pagination", () => {
     cy.visit("https://mykoladudko.netlify.app");
     cy.get('[data-index="1"] > a').click();
     cy.get("#nextBtn").click();
+
     cy.get("#nextBtn").click();
+
     cy.get("#projectCard9 > picture > img.project-img").should("be.visible");
     cy.get("#projectCard10 > picture > img.project-img").should("be.visible");
     cy.get("#projectCard11 > picture > img.project-img").should("be.visible");
     cy.get("#projectCard12 > picture > img.project-img").should("be.visible");
+
     cy.get("#prevBtn").click();
+
     cy.get("#projectCard5 > picture > img.project-img").should("be.visible");
     cy.get("#projectCard6 > picture > img.project-img").should("be.visible");
     cy.get("#projectCard7 > picture > img.project-img").should("be.visible");
     cy.get("#projectCard8 > picture > img.project-img").should("be.visible");
+
     cy.get("#prevBtn").click();
+
     cy.get("#projectCard4 > picture > img.project-img").should("be.visible");
     cy.get("#projectCard3 > picture > img.project-img").should("be.visible");
     cy.get("#projectCard2 > picture > img.project-img").should("be.visible");
     cy.get("#projectCard1 > picture > img.project-img")
       .trigger("mouseover")
-      .wait(1000); 
-      cy.get('[data-index="2"] > a').click();
-      cy.wait(4000);
+      .wait(1000);
+
+    cy.get('[data-index="2"] > a').click();
+    cy.wait(4000);
   });
-
- 
-
 });
